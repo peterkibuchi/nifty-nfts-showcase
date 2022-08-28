7c6782622ef9f0b30fdb6f76d385f5d72075eb7f
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2022-08-28 07:15:42 +0300

    Complete Features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,34 @@
+import assets from "../assets";
+import styles from "../styles/Global";
+
+const FeatureCard = ({ iconUrl, iconText }) => (
+  <div className={styles.featureCard}>
+    <img src={iconUrl} alt="icon" className={styles.featureImg} />
+    <p className={styles.featureText}>{iconText}</p>
+  </div>
+);
+
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className={`${styles.section} ${styles.bgPrimary} banner03`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h2 className={`${styles.h1Text} ${styles.whiteText}`}>
+            Technologies
+          </h2>
+          <p className={`${styles.pText} ${styles.whiteText}`}>
+            ProNef has been developed using React Native, a cross-platform
+            technology that enables it to run on both Android and iOS devices.
+          </p>
+        </div>
+
+        <div className={`${styles.flexWrap}`}>
+          <FeatureCard iconUrl={assets.react} iconText="React Native" />
+          <FeatureCard iconUrl={assets.javascript} iconText="JavaDcript" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
