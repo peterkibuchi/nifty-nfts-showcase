ea3088e55dfded3236bf6715164dd1fd2c4ebd27
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2022-08-28 07:15:25 +0300

    Complete Download

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,5 +1,39 @@
+import assets from "../assets";
+import styles from "../styles/Global";
+
 const Download = () => {
-  return <div>Download</div>;
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h2 className={`${styles.h1Text} ${styles.blackText}`}>
+            Download the Source Code
+          </h2>
+          <p className={`${styles.pText} ${styles.blackText}`}>
+            Get the full source code on GitHub
+          </p>
+        </div>
+
+        <button className={`${styles.btnPrimary}`}>
+          <a
+            href="https://github.com/peterkibuchi/nifty-nfts"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Source Code
+          </a>
+        </button>
+
+        <div className={`${styles.flexCenter}`}>
+          <img
+            src={assets.scene}
+            alt="download_png"
+            className={`${styles.fullImg}`}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Download;
